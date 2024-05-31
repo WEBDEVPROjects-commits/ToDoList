f3fb41053379d0f2e5ddf714c325034ef76dd24a
harshit-bca1 <130763950+harshit-bca1@users.noreply.github.com>
2024-05-31 13:04:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.querySelector(".content").children[1].addEventListener("click", (e) => {
+    var x = document.querySelector(".content").children[0].value
+    var html = `<div class="task">
+    <input type="checkbox" id="checkbox" >
+    <p>${x}</p>
+    <img src=".png" alt="image" class="bin-image">
+    </div>`
+    document.querySelector(".container").insertAdjacentHTML("beforeend", html)
+    document.querySelector(".container").lastElementChild.children[2].addEventListener("click", (e) => {
+        e.target.closest(".task").remove();
+    }
+    )
+})
